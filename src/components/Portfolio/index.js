7085aaebf14b9b0dc29ce2480b25a1e3ffdf6cf1
sdpercy <scottpercy@hotmail.com>
2021-09-04 10:20:42 -0230

    fixed project errors

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,72 @@
 import React from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import Project from '../Project';
 
-function Portfolio(props) {
-  const { currentCategory } = props;
+function Portfolio() {
+  const projects = [
+    //---------template-----------
+    // {
+    //   name: '', 
+    //   description:'', 
+    //   image:'', 
+    //   technology: [],  
+    //   deployedApp:'',
+    //   repo:'',
+    // },
+    {
+      name: 'WASTEAWAY', 
+      description:'A product waste managing web application which allows business owners to manage their kitchen waste in order to lower their overhead costs and also decrease their carbon footprint.', 
+      image:'wasteaway.JPG', 
+      technology: ['Node.js',
+        'Express.js',
+        'Handlebars.js',
+        'MySQL',
+        'Sequelize ORM',
+        'jQuery',
+        'dotenv',],  
+      deployedApp:'https://waste-management-project2.herokuapp.com/',
+      repo:'https://github.com/Group05-Project02/waste-management',
+    },
+    {
+      name: '', 
+      description:'', 
+      image:'', 
+      technology: [],  
+      deployedApp:'',
+      repo:'',
+    },
+  ];
   return (
     <section>
-     <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
-     <p>{currentCategory.description}</p>
-     
+      <div>
+        <h1 className="pageTitle">Portfolio</h1>
+      </div>
+        <ul className="flex-row">
+          <li>
+            <Project projects={projects[0]}></Project>
+          </li>
+          <li>
+            <Project projects={projects[1]}></Project>
+          </li>
+        </ul>
+        <ul className="flex-row">
+          <li>
+            <Project projects={projects[2]}></Project>
+          </li>
+          <li>
+            <Project projects={projects[3]}></Project>
+          </li>
+        </ul>
+        <ul className="flex-row">
+          <li>
+            <Project projects={projects[4]}></Project>
+          </li>
+          <li>
+            <Project projects={projects[5]}></Project>
+          </li>
+        </ul>
+
     </section>
-  );
+  )
 }
+
 export default Portfolio;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 function Project(props) {
-    const currentproject = useState(props)[0].projects;
+    const currentProject = useState(props)[0].projects;
 
     const name = currentProject.name;
     const description = currentProject.description;
@@ -27,7 +27,7 @@ function Project(props) {
 
     return (
         <Card style={{ width: "15rem"}}>
-            <Card.Img variant="top" src={require(`../../images/{images}`)} className="card-image" />
+            <Card.Img variant="top" src={require(`../../assets/images/${image}`)} className="card-image" />
 
             <div>
                 <Card.Body>
