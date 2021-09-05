65751aee92dbbd5fca730ae4f41f8d9b9554d0dd
sdpercy <scottpercy@hotmail.com>
2021-09-05 11:07:58 -0230

    fixed navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,15 +14,18 @@ function Nav(props) {
   return (
       <nav>
             <ul className="flex-row">
-                 <li className= {currentCategory === "about" ? "mx-2 navActive" : "mx-2"}>
+                 <li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
                     <span onClick={() => setCurrentCategory("about")}> About Me </span>
                 </li>
+
                 <li className= {currentCategory === "portfolio" ? "mx-2 navActive" : "mx-2"}>
                     <span onClick={() => setCurrentCategory("portfolio")}> Portfolio </span>
                 </li>
+
                 <li className= {currentCategory === "contact" ? "mx-2 navActive" : "mx-2"}>
                     <span onClick={() => setCurrentCategory("contact")}> Contact </span>
                 </li>
+
                 <li className= {currentCategory === "resume" ? "mx-2 navActive" : "mx-2"}>
                     <span onClick={() => setCurrentCategory("resume")}> Resume </span>
                 </li> 
