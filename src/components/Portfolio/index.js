137919aec4126be11297c014a66ad8a8b6286776
sdpercy <scottpercy@hotmail.com>
2021-09-06 22:53:56 -0230

    added portfolio entries

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -42,6 +42,53 @@ function Portfolio() {
         ],  
       deployedApp: 'https://foodisfuel.github.io/gp5_project1/',
       repo:'https://github.com/foodisfuel/gp5_project1'
+    },
+    {
+      name: 'Note Taker', 
+      description:'This application allows a user to create, save and delete notes to help organize the users day to keep track of tasks that need to be completed. This application use an express backend to save and retrieve note data from a JSON file.', 
+      image: 'notetaker.png', 
+      technology: ['HTML',
+        'Javascript',
+        'CSS',
+        
+        ],  
+      deployedApp: 'https://true-mountie-19014.herokuapp.com/',
+      repo:'https://github.com/sdpercy/note-taker'
+    },
+    {
+      name: 'Budget Tracker', 
+      description:'Budget track able to track withdrawals and deposits with or without a data/internet connection SO THAT my account balance is accurate when I am traveling', 
+      image: 'budgetracker.png', 
+      technology: ['Node.js',
+        'Express',
+        'MongoDB',
+        'Mongoose',
+        ],  
+      deployedApp: 'https://dry-shelf-29380.herokuapp.com/',
+      repo:'https://github.com/sdpercy/pwa-budget-tracker'
+    },
+    {
+      name: 'Weather Dashboard', 
+      description:'Weather application to search for cities and get current weather, 5 day forcast and UV index.', 
+      image: 'weatherdashboard.png', 
+      technology: ['HTML',
+        'Bootstrap',
+        'jQuery',
+        'Javascript',
+        'Open Weather API'
+        ],  
+      deployedApp: 'https://sdpercy.github.io/weather-dashboard/',
+      repo:'https://github.com/sdpercy/weather-dashboard'
+    },
+    {
+      name: 'Run-Buddy', 
+      description:'A website that offers fitness training services.', 
+      image: 'run_buddy_website.jpg', 
+      technology: ['HTML',
+        'CSS'
+        ],  
+      deployedApp: 'https://lernantino.github.io/run-buddy/',
+      repo:'https://github.com/sdpercy/run-buddy'
     }
   ];
   return (
@@ -54,7 +101,7 @@ function Portfolio() {
             <Project projects={projects[1]}></Project>
           </li> 
         </ul>
-        {/* <ul className="flex-row">
+        <ul className="flex-row">
           <li>
             <Project projects={projects[2]}></Project>
           </li>
@@ -69,7 +116,7 @@ function Portfolio() {
           <li>
             <Project projects={projects[5]}></Project>
           </li>
-        </ul> */}
+        </ul>
     </section>
   );
 }
